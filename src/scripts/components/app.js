33e91fef13fd43197dff3028a796061ea830f6f6
Welch Canavan <welch.canavan@gmail.com>
2017-03-02 18:38:22 -0500

    style display type

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Será</h1>
+        <h1 className="type-display">Será</h1>
         <Releases />
         <Shows />
       </div>
